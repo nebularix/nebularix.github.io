689f27f9d89fef47d0c8af6a2861dec0c54c866c
nebularix <samfurtaw+1@gmail.com>
2023-11-21 10:20:16 -0500

    Fix scaling and mobile issues

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function switchGallery() {
     document.getElementById("mainContent").style.setProperty("display", "none");
-    document.getElementById("gallery").style.setProperty("display", "inline");
+    document.getElementById("gallery").style.setProperty("display", "block");
 }
 
 function switchMain() {
-    document.getElementById("mainContent").style.setProperty("display", "inline");
+    document.getElementById("mainContent").style.setProperty("display", "block");
     document.getElementById("gallery").style.setProperty("display", "none");
 }
